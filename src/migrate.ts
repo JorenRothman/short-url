a3acb9abe1a2fad00331158604451bdd80d7c1fa
Joren Rothman <joren@instance.studio>
2024-03-01 09:40:51 +0100

    Fixed migrate file

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,7 +1,9 @@
-import { connection, db } from '@/server/db';
+import { getDB } from '@/server/db';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 
 async function migrateDB() {
+    const { db, connection } = getDB();
+
     await migrate(db, { migrationsFolder: './drizzle' });
 
     await connection.end();
