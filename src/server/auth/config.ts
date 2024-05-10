f3d93140ad27bb50e2f9ff7da65f8e7b021a1e26
Joren Rothman <joren@instance.studio>
2024-05-10 18:56:46 +0200

    Update

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,9 +1,9 @@
-import { DrizzleMySQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { users, sessions } from "@/schema";
 import { db } from "@/server/db";
 import { Lucia } from "lucia";
 
-const adapter = new DrizzleMySQLAdapter(db, sessions, users);
+const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
 
 export const lucia = new Lucia(adapter, {
     sessionCookie: {
